4e6dcb04ea5f9754a653fea4f4cb6ae0bfece180
Montané Hamilton <montanehamilton@hotmail.com>
2017-01-03 13:59:41 -0700

    New scripts for CMP

---
+++ PowerShell/ConfigMgrPortal/ConfigMgrPortal.ps1
@@ -0,0 +1,87 @@
+﻿function DownloadFile([System.Uri]$uri, $destinationDirectory){
+    $fileName = $uri.Segments[$uri.Segments.Count-1]
+    $destinationFile = Join-Path $destinationDirectory $fileName
+
+    "Downloading $uri to $destinationFile"
+
+    $webclient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($uri,$destinationFile)
+}
+
+function Get-DeploymentScripts($destinationFolder, $uris){
+	Write-Host "Start Get-DeploymentScripts" -ForegroundColor Green
+	if((Test-Path $destinationFolder) -ne $true){
+        $newItem = New-Item $destinationFolder -ItemType Directory
+		Write-Host "Created destination $destinationFolder" -ForegroundColor Green
+    }else{
+        Remove-Item -Path "$destinationFolder\*" -Recurse -Force
+		Write-Host "Cleaned destination $destinationFolder" -ForegroundColor Green
+    }
+
+	foreach($uri in $uris){
+		DownloadFile -uri $uri -destinationDirectory $destinationFolder
+	}
+
+	Write-Host "End Get-DeploymentScripts" -ForegroundColor Green
+}
+
+function Start-Deployment($agentPowerShellLocation, $powershellDirectoryName, $dependentPackages){
+	$ErrorActionPreference = "Stop"
+	Write-Host "************************************************************************"
+	Write-Host "Start-Deployment Version 1.0.0" -ForegroundColor Yellow
+
+	$deploymentVariables = @{
+		targetMachineHostName = $Env:targetMachineHostName
+		targetMachineUserName = $Env:targetMachineUserName
+		targetMachinePassword = $Env:targetMachinePassword
+		resourceGroupName = $Env:resourceGroupName 
+		storageAccountName = $Env:storageAccountName  
+		storageTempContainerName = $Env:storageTempContainerName
+		agentReleaseDirectory = $Env:AGENT_RELEASEDIRECTORY
+		buildDefinitionName = $Env:BUILD_DEFINITIONNAME
+		targetVersion = $env:BUILD_BUILDNUMBER
+		vstsAccountName = $Env:vstsAccountName
+		vstsApiUserName = $Env:vstsApiUserName
+		vstsApiPassword = $Env:vstsApiPassword
+	}
+
+	Write-Host "Environment Variables Copied to HashTable`r`n" -ForegroundColor Green
+	foreach($key in $deploymentVariables.keys){
+		$value = $deploymentVariables[$key]
+		Write-Host "$key`: $value" -ForegroundColor Green
+	}
+
+	$deploymentScripts = @(
+		[System.Uri]"https://raw.githubusercontent.com/Cireson/DeploymentTools/master/PowerShell/ConfigMgrPortal/WebsiteSetup.ps1"
+		[System.Uri]"https://raw.githubusercontent.com/Cireson/DeploymentTools/master/PowerShell/Utility.ps1"
+		[System.Uri]"https://raw.githubusercontent.com/Cireson/DeploymentTools/master/PowerShell/CommonDeployment.ps1"
+	)
+
+	Get-DeploymentScripts $agentPowerShellLocation $deploymentScripts
+
+	foreach($uri in $deploymentScripts){
+		$fileName = $uri.Segments[$uri.Segments.Count-1]
+		$module = "$agentPowerShellLocation\$fileName"
+		Write-Host "`tImporting module $module" -ForegroundColor Green
+		Import-Module $module
+	}
+
+	$remotePowerShellLocation = "c:\$powershellDirectoryName"
+	$session = Create-RemoteSession $deploymentVariables.targetMachineHostName $deploymentVariables.targetMachineUserName $deploymentVariables.targetMachinePassword
+
+	Ensure-EmptyRemoteDirectoryExists -session $session -directory $remotePowerShellLocation 
+
+	Push-RemoteDeploymentScripts $session $deploymentScripts $remotePowerShellLocation
+
+	$remoteScript = $remotePowerShellLocation + "\WebsiteSetup.ps1"
+	$setupValues = @{
+		Version = $deploymentVariables.targetVersion
+	}
+
+
+	Invoke-Command -Session $session -FilePath $remoteScript -ArgumentList $setupValues { 
+		Write-Host "************************************************************************"
+        
+		Write-Host "End running on remote machine, $targetMachineHostName."
+    }
+}
---
+++ PowerShell/ConfigMgrPortal/WebsiteSetup.ps1
@@ -0,0 +1,160 @@
+Param(
+	$setupValues
+)
+
+function CreateOrUpdateWebsite($newWebsitePath){
+    [Void][Reflection.Assembly]::LoadWithPartialName("Microsoft.Web.Administration")
+
+    $currentWebsitePath = ""
+    $websiteName = "ConfigMgrPortal"
+
+    $serverManager = New-Object Microsoft.Web.Administration.ServerManager
+
+    # Find current version from IIS
+    $site = $serverManager.Sites | Where-Object {$_.name -eq $websiteName}
+
+    if($site -eq $null){
+        # If no current version setup site for first time.
+        New-WebAppPool -Name $websiteName
+        Write-Host "Setup application pool."
+        New-Website -Name $websiteName -PhysicalPath $newWebsitePath -ApplicationPool $websiteName -Port 80 -HostHeader "integration-configmgrportal.cireson.com"
+        Set-WebConfiguration system.webServer/security/authentication/windowsAuthentication -PSPath IIS:\ -Location $websiteName -Value @{enabled="True"}
+        Set-WebConfiguration system.webServer/security/authentication/anonymousAuthentication -PSPath IIS:\ -Location $websiteName -Value @{enabled="False"}
+        Write-Host "Setup new web site."
+    }else{
+        $rootApp = $site.Applications | where-object { $_.Path -eq "/" }
+        $rootApp.VirtualDirectories | where { $_.Path -eq "/" }
+        $rootVdir = $rootApp.VirtualDirectories | where { $_.Path -eq "/" }
+        $currentWebsitePath = $rootVdir.PhysicalPath;
+
+        # Otherwise copy Configuration.xml and Web.Config from old over new.
+        $configFileSource = $currentWebsitePath + "\configuration.xml"
+        $configFileTarget = $newWebsitePath + "\configuration.xml"
+        if((Test-Path $configFileSource) -eq $true -and $configFileSource -ne $configFileTarget){
+            Copy-Item -path $configFileSource -Destination $configFileTarget -Force
+            Write-Host "Copied existing Configuration.xml to new site."
+        }
+
+        $webConfigFileSource = $currentWebsitePath + "\web.config"
+        $webConfigFileTarget = $newWebsitePath + "\web.config"
+        if((Test-Path $webConfigFileSource) -eq $true -and $webConfigFileSource -ne $webConfigFileTarget){
+            Copy-Item -path $webConfigFileSource -Destination $webConfigFileTarget -Force
+            Write-Host "Copied existing Web.config to new site."
+        }
+
+        # Point website at new folder
+        $rootVdir.PhysicalPath = $newWebsitePath
+        $serverManager.CommitChanges()
+        Write-Host "Updated existing site's physical path"
+    }
+}
+
+function Update-ServiceConfiguration($serviceRoot, $websiteRoot){
+    $configFile = $serviceRoot + "\ConfigMgr Portal Hosting Service.exe.config"
+    [xml] $xml = Get-Content $configFile
+    $xml.SelectSingleNode("/configuration/appSettings/add[@key='BaseFolder']").value = $websiteRoot
+    $xml.Save($configFile)
+}
+
+function Get-WebsiteDeploymentInfo($version){
+    $websiteDeployPath = "c:\websites"
+
+    if((Test-Path -Path $websiteDeployPath) -eq $false){
+        New-Item -Path $websiteDeployPath -ItemType Directory
+    }
+
+    $websiteDeploymentPath = $websiteDeployPath + "\deployment"
+    if((Test-Path -Path $websiteDeploymentPath) -eq $false){
+        New-Item -Path $websiteDeploymentPath -ItemType Directory
+    }
+    
+    $websiteDeployPath = $websiteDeployPath + "\cmpWebsite"
+
+    if((Test-Path -Path $websiteDeployPath) -eq $false){
+        New-Item -Path $websiteDeployPath -ItemType Directory
+    }
+
+    $websiteDeployPath = $websiteDeployPath + "\" + $version
+
+    if((Test-Path -Path $websiteDeployPath) -eq $true){
+        Remove-Item $websiteDeployPath -Recurse -Force
+    }
+    New-Item -Path $websiteDeployPath -ItemType Directory
+
+    $websiteSourcePath = ""
+    $files = ls $websiteDeploymentPath
+    $file = $files | Where-Object{ $_.Extension -eq ".zip"} | Select-Object -first 1
+    $file
+
+    if($file -ne $null){
+        $websiteSourcePath = $websiteDeploymentPath + "\extracted"
+        if((Test-Path -Path $websiteSourcePath) -eq $true){
+            Remove-Item $websiteSourcePath -Recurse -Force
+        }
+
+        New-Item -Path $websiteSourcePath -ItemType Directory
+
+        Unzip-File -zipfile $file.FullName -outpath $websiteSourcePath
+        $websiteSourcePath = $websiteSourcePath + "\website\*"
+    }
+
+    return @{
+        SourcePath = $websiteSourcePath
+        DeployPath = $websiteDeployPath
+    }
+}
+
+$version = $setupValues.Version
+$websiteInfo = Get-WebsiteDeploymentInfo -version $version
+Copy-Item -Path $websiteInfo.SourcePath -Destination $websiteInfo.DeployPath -Recurse
+CreateOrUpdateWebsite -newWebsitePath $websiteInfo.DeployPath
+
+$serviceDeployPath = "c:\services"
+
+if((Test-Path -Path $serviceDeployPath) -eq $false){
+    New-Item -Path $serviceDeployPath -ItemType Directory
+}
+
+$serviceSourcePath = $serviceDeployPath + "\deployment"
+if((Test-Path -Path $serviceSourcePath) -eq $false){
+    New-Item -Path $serviceSourcePath -ItemType Directory
+}
+
+$serviceDeployPath = $serviceDeployPath + "\cmpService"
+
+if((Test-Path -Path $serviceDeployPath) -eq $false){
+    New-Item -Path $serviceDeployPath -ItemType Directory
+}
+
+$serviceDeployPath = $serviceDeployPath + "\" + $version
+
+if((Test-Path -Path $serviceDeployPath) -eq $true){
+    Remove-Item $serviceDeployPath -Recurse -Force
+}
+New-Item -Path $serviceDeployPath -ItemType Directory
+
+$serviceSourcePath = $serviceSourcePath + "\Cireson ConfigMgr Portal Service x64.msi"
+
+Copy-Item -Path $serviceSourcePath -Destination $serviceDeployPath
+
+$serviceMsi = $serviceDeployPath + "\Cireson ConfigMgr Portal Service x64.msi"
+
+# run the msi
+#msiexec.exe /i '$serviceMsi' /qn /l*v c:\Temp\logfile.log ALLUSERS=2    
+$arguments = @(
+    "/i"
+    "`"$serviceMsi`""
+    "/qn"
+)
+$process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+if($process.ExitCode -eq 0){
+    Write-Host "Installed Service"
+}
+    
+$serviceDeployPath = "$Env:ProgramFiles\Cireson\Portal for Configuration Manager\Services"
+
+# Update ConfigMgr Portal Hosting Service.exe.config, setting the BaseFolder to the new folder for website
+Update-ServiceConfiguration -serviceRoot $serviceDeployPath -websiteRoot $websiteInfo.DeployPath
+
+# Start Portal Service
+Start-Service -Name "Cireson ConfigMgr Portal Hosting Service"
